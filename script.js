5de0ea0b849281be287d60504de68742fbf0fa15
Bruna Felix <96153601+brunafelix79@users.noreply.github.com>
2022-04-30 10:58:20 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+const img = document.getElementById( 'img' );
+const buttons = document.getElementById( 'buttons' );
+let colorIndex = 0;
+let intervalId = null;
+
+const trafficLight = ( event ) => {
+    stopAutomatic();
+    turnOn[event.target.id]();
+}
+
+const stopAutomatic = () => {
+    clearInterval ( intervalId );
+}
+
+const turnOn = {
+    'red':      () => img.src = './img/vermelho.png',
+    'yellow':   () => img.src = './img/amarelo.png',
+    'green':    () => img.src = './img/verde.png',
+   
+}
+
+buttons.addEventListener('click', trafficLight );
+
